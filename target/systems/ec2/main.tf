88604ec84d036b3bafb5c358a0f9a2eca76d3b3a
KingMoses33 <136907140+KingMoses33@users.noreply.github.com>
2024-03-01 10:32:06 -0500

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "server" {
+  source        = "https://github.com/etechteam6/base-rac-tf/tree/main//modules/ec2"
+  ami           = "ami-0440d3b780d96b29d"
+  instance_type = "t2.medium"
+  num           = 10
+}
